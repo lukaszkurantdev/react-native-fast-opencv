55289a7c91f7524609f908294021662cc20fcf44
Łukasz Kurant <lukaslux@outlook.com>
2024-08-24 15:24:00 +0200

    chore: setup action update

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,4 +3,8 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.jsx',
 });
 
-module.exports = withNextra();
+module.exports = {
+  ...withNextra({
+    output: 'export',
+  }),
+};
