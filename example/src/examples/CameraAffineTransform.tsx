1963dbce7f8bc7b046b5a1e1c88f6ff8551570c3
Thomas Coldwell <31568400+thomas-coldwell@users.noreply.github.com>
2024-09-27 09:31:42 +0100

    fix: remove unused

--- example/src/examples/CameraAffineTransform.tsx
+++ example/src/examples/CameraAffineTransform.tsx
@@ -88,23 +88,6 @@ export function createSkiaImageFromData(
   );
 }
 
-// const multiply2x3Matrix = (a: number[][], b: number[][]) => {
-//   'worklet';
-//   const result = [];
-//   for (let i = 0; i < a.length; i++) {
-//     const row = [];
-//     for (let j = 0; j < b[0].length; j++) {
-//       let sum = 0;
-//       for (let k = 0; k < a[0].length; k++) {
-//         sum += a[i][k] * b[k][j];
-//       }
-//       row.push(sum);
-//     }
-//     result.push(row);
-//   }
-//   return result;
-// };
-
 export function CameraAffineTransform() {
   const device = useCameraDevice('back');
   const { hasPermission, requestPermission } = useCameraPermission();
